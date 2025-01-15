81ad60f5efbc31f14e0b56b22af38a4ea2ffff2f
Stephen Williams <stephen@wearetmbr.com>
2025-01-15 13:26:32 -0700

    parcel test

--- wp-content/themes/snowpack/functions.php
+++ wp-content/themes/snowpack/functions.php
@@ -1,7 +1,7 @@
 <?php 
 function snowpack_scripts() {
-  wp_enqueue_style( 'snowpack-style', get_template_directory_uri() . '/prod/scripts.css.map', array(), '1.0.0', 'all' );
-  wp_enqueue_style( 'snowpack-script', get_template_directory_uri() . '/prod/scripts.js.map', array(), '1.0.0', 'all' );
+  wp_enqueue_style( 'snowpack-style', get_template_directory_uri() . '/prod/scripts.css');
+  wp_enqueue_style( 'snowpack-script', get_template_directory_uri() . '/prod/scripts.js');
 }
 
 add_action( 'wp_enqueue_scripts', 'snowpack_scripts' );
